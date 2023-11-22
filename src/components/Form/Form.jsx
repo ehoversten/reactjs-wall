763572c85c40887e491d70c221e6cc6ff35eb3f8
Erik Hoversten <ehoversten@gmail.com>
2023-11-21 20:15:44 -0500

    added basic form component logic

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,8 +1,26 @@
 import React, { useState } from 'react'
 
-function Form() {
+function Form({ add }) {
+
+    const [newReminder, setNewReminder] = useState('');
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        add(newReminder);
+        setNewReminder('')
+    }
+
   return (
-    <div className='form-container'>Form</div>
+    <div className='form-container'>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="reminder">Add New Reminder:</label>
+            <input type="text" 
+                   id="reminder"
+                   value={newReminder} 
+                   onChange={(evt) => setNewReminder(evt.target.value)}/>
+            <button type="submit">Add</button>
+        </form>
+    </div>
   )
 }
 
