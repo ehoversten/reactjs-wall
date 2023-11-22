03486c162804a105c649104953faa4969a58532f
Erik Hoversten <ehoversten@gmail.com>
2023-11-21 20:04:13 -0500

    created basic logic for list container

--- src/components/ListContainer/ListContainer.jsx
+++ src/components/ListContainer/ListContainer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-function ListContainer() {
+function ListContainer({ data }) {
   return (
-    <div className='list-container'>ListContainer</div>
+    <div className='list-container'>
+        <h2>List Container</h2>
+        <div className="data-container">
+            { data && data.map((item, index) => (
+                <p key={index}>{item}</p>
+            ))}
+        </div>
+    </div>
   )
 }
 
--- src/pages/Whiteboard.jsx
+++ src/pages/Whiteboard.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Form from '../components/Form/Form'
+import ListContainer from '../components/ListContainer/ListContainer'
 
 function Whiteboard() {
+    const [reminders, setReminders] = useState(['one', 'two']);
+
+    const addReminder = (item) => {
+        setReminders(prev => [...prev, item]);
+    }
+
   return (
-    <div className='whiteboard-container'>Whiteboard</div>
+    <div className='whiteboard-container'>
+        <h2>Whiteboard</h2>
+        <Form add={addReminder} />
+        <ListContainer data={reminders}/>
+    </div>
   )
 }
 
