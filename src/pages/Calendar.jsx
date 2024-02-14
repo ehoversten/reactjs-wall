2e4453a9552b0c69365be83b90eb8c0f3468789a
Erik Hoversten <ehoversten@gmail.com>
2024-02-13 21:00:40 -0500

    completed basic calendar layout

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { format, startOfMonth, endOfMonth, eachDayOfInterval } from 'date-fns';
+import clsx from 'clsx';
+import { format, startOfMonth, endOfMonth, eachDayOfInterval, getDay, isToday } from 'date-fns';
 
 function Calendar() {
     const currentDate = new Date();
@@ -10,18 +11,30 @@ function Calendar() {
     const daysInMonth = eachDayOfInterval({
         start: firstDayOfMonth,
         end: lastDayOfMonth,
-    })
+    });
+
+    const startingDayIndex = getDay(firstDayOfMonth);
+
   return (
-    <div className="event-container">
-        <div>
-            <h2 className='text-center'>{format(currentDate, "MMMM yyyy")}</h2>
+    <div className="event-container container mx-auto p-4">
+        <div className="mb-4">
+            <h2 className="text-center">{format(currentDate, "MMMM yyyy")}</h2>
         </div>
-        <div className="grid-container">
+        <div className="grid grid-cols-7 gap-2">
             {WEEKDAYS.map(day => {
                 return <div key={day}>{day}</div>
             })}
+            {Array.from({ length: startingDayIndex }).map((_, index) => {
+                return <div key={`empty-${index}`}>X</div>
+            })}
             {daysInMonth.map((day, index) => {
-                return <div key={index}>{format(day, "d")}</div>
+                return <div 
+                            key={index}
+                            className={clsx("border rounded-md p-2 text-center", {
+                                "bg-gray-200": isToday(day),
+                                "text-gray-900": isToday(day)
+                            })}
+                        >{format(day, "d")}</div>
             })}
         </div>
     </div>
